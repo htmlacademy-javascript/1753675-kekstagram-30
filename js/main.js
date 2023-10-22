00a0777a07b2d6ac9f7f8f9798bee6feb3dfcb4a
Anastasia <mrgabrielripper@gmail.com>
2023-10-22 21:53:43 +0600

    Подключает модуль отрисовки фото-галереи

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {generatePhotoArray} from './generate-data.js';
+import {renderThumbnails} from './photo-gallery.js';
 
 const OBJECTS_NUM = 25; // количество сгенерированных объектов
+const photoArray = generatePhotoArray(OBJECTS_NUM); // генерируем массив с данными и сохраняем
 
-generatePhotoArray(OBJECTS_NUM);
+renderThumbnails(photoArray);
