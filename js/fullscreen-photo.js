1668a84236c7ea3e777652d3b832a93cc6fae83b
Anastasia <mrgabrielripper@gmail.com>
2023-10-28 16:03:20 +0600

    Вносит правки в нейминг, добавляет комментарии

--- js/fullscreen-photo.js
+++ js/fullscreen-photo.js
@@ -1,5 +1,6 @@
 import {isEscapeKey} from './utils.js';
 
+// Получение ссылок на элементы
 const fullSizePhoto = document.querySelector('.big-picture');
 const fullSizePhotoImg = fullSizePhoto.querySelector('.big-picture__img img');
 const likesCount = fullSizePhoto.querySelector('.likes-count');
@@ -10,30 +11,33 @@ const photoDescription = fullSizePhoto.querySelector('.social__caption');
 const picturesContainer = document.querySelector('.pictures');
 const fullSizePhotoCloseBtn = fullSizePhoto.querySelector('.big-picture__cancel');
 
+// Обрабатываем событие нажатия клавиши на документе
 const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt)) { // Проверяем является ли нажатая клавиша Escape
     evt.preventDefault();
     closeFullSizePhotoModal();
   }
 };
 
+// Закрываем полноразмерное фото
 const closeFullSizePhotoModal = () => {
   fullSizePhoto.classList.add('hidden');
-  document.body.classList.remove('modal-open');
+  document.body.classList.remove('modal-open'); // Удаляем класс для блокировки прокрутки страницы
   document.body.removeEventListener('keydown', onDocumentKeydown);
 };
 
-const openFullSizePhotoModal = (dataItem) => {
+// Открываем полноразмерное фото, передавая объект с данными изображения
+const openFullSizePhotoModal = (data) => {
   fullSizePhoto.classList.remove('hidden');
-  fullSizePhotoImg.src = dataItem.url;
-  photoDescription.textContent = dataItem.description;
-  likesCount.textContent = dataItem.likes;
-  commentsShownCount.textContent = dataItem.comments.length;
-  commentsTotalCount.textContent = dataItem.comments.length;
+  fullSizePhotoImg.src = data.url;
+  photoDescription.textContent = data.description;
+  likesCount.textContent = data.likes;
+  commentsShownCount.textContent = data.comments.length;
+  commentsTotalCount.textContent = data.comments.length;
   commentsList.innerHTML = '';
 
   // отрисовку комментариев нужно вынести в отдельный модуль
-  dataItem.comments.forEach((comment) => {
+  data.comments.forEach((comment) => {
     const commentItem = document.createElement('li');
     commentItem.classList.add('social__comment');
     commentItem.innerHTML = `<img class="social__picture" src="${comment.avatar}" alt="" width="35" height="35">
@@ -46,18 +50,19 @@ const openFullSizePhotoModal = (dataItem) => {
   commentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
 
-  document.body.classList.add('modal-open');
+  document.body.classList.add('modal-open'); // Добавляем класс для блокировки прокрутки страницы
 
   fullSizePhotoCloseBtn.addEventListener('click', closeFullSizePhotoModal);
   document.body.addEventListener('keydown', onDocumentKeydown);
 };
 
+// Обрабатываем событие клика на миниатюрах
 const onThumbnailClick = (data) => {
   picturesContainer.addEventListener('click', (evt) => {
-    const thumbnailLink = evt.target.closest('a.picture');
+    const thumbnailLink = evt.target.closest('a.picture'); // Ищем ближайший родительский элемент по селектору
     if (thumbnailLink) {
       evt.preventDefault();
-      const thumbnailIndex = data[thumbnailLink.dataset.index];
+      const thumbnailIndex = data[thumbnailLink.dataset.index]; // Получаем индекс миниатюры из атрибута data-index
       if (thumbnailIndex) {
         openFullSizePhotoModal(thumbnailIndex);
       }
