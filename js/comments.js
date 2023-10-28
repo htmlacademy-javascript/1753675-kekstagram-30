32aee0e0d0dc83e2416bfb1ccbf3cb8447be03ae
Anastasia <mrgabrielripper@gmail.com>
2023-10-28 16:50:25 +0600

    Добавляет комментарии к модулю

--- js/comments.js
+++ js/comments.js
@@ -1,18 +1,24 @@
+// Находим контейнер для комментариев и шаблон комментария
 const commentsList = document.querySelector('.social__comments');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
+// Отображаем комментарии на странице
 const renderComments = (array) => {
+  // Создаем фрагмент для добавления комментариев
   const commentFragment = document.createDocumentFragment();
+  // Очищаем контейнер с комментариями
   commentsList.innerHTML = '';
 
   array.forEach(({avatar, message, name}) => {
     const commentElement = commentTemplate.cloneNode(true);
+    // Устанавливаем значения атрибутов и текстовое содержимое
     commentElement.querySelector('.social__picture').setAttribute('src', avatar);
     commentElement.querySelector('.social__picture').setAttribute('alt', name);
     commentElement.querySelector('.social__text').textContent = message;
     commentFragment.appendChild(commentElement);
   });
 
+  // Добавляем фрагмент с комментариями в контейнер
   commentsList.appendChild(commentFragment);
   // Скрываем счетчик комментариев и кнопку загрузки новых комментариев
   const commentCount = document.querySelector('.social__comment-count');
