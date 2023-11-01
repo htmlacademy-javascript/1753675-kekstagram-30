d09cea8372cb7215206eeccf1d044218bdf84098
Anastasia <mrgabrielripper@gmail.com>
2023-11-01 22:20:26 +0600

    Добавляет браузерный API для подстановки загруженного изображения в превью

--- js/form.js
+++ js/form.js
@@ -4,8 +4,14 @@ const uploadImageForm = document.querySelector('.img-upload__form');
 const uploadInput = uploadImageForm.querySelector('.img-upload__input');
 const overlay = uploadImageForm.querySelector('.img-upload__overlay');
 const cancelButton = uploadImageForm.querySelector('.img-upload__cancel');
+const uploadImagePreview = document.querySelector('.img-upload__preview > img');
 
 const handleImageUpload = () => {
+  const reader = new FileReader();
+  reader.onload = function (evt) {
+    uploadImagePreview.src = evt.target.result;
+  };
+  reader.readAsDataURL(uploadInput.files[0]);
   overlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
 };
