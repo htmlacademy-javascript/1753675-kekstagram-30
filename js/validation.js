e0da32ca43cc83ac59fbe881b576020e16382e4f
Anastasia <mrgabrielripper@gmail.com>
2023-11-06 13:37:13 +0600

    Исправляет поведение функции для валидирования хэштегов

--- js/validation.js
+++ js/validation.js
@@ -43,7 +43,6 @@ const validateHashtags = (value) => {
       if (!invalidData.hashtags.includes(errorMessage.hasDuplicates)) {
         invalidData.hashtags.push(errorMessage.hasDuplicates);
       }
-      console.log(invalidData.hashtags);
       continue;
     }
 
